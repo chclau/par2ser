31affb858954547290f19e6c95d0a7c30a9314ca
Claudio Avi Chami <chclau@yahoo.com>
2022-10-13 15:13:29 +0300

    Add files via upload

--- src/par2ser.vhd
+++ src/par2ser.vhd
@@ -2,90 +2,75 @@
 -- Company:  FPGA'er
 -- Engineer: Claudio Avi Chami - FPGA'er Website
 --           http://fpgaer.tech
--- Create Date: 11.09.2022 
+-- Create Date: 01.10.2022 
 -- Module Name: par2ser.vhd
 -- Description: Parallel to serial converter
 -- Dependencies: none
 -- 
--- Revision: 1
+-- Revision: 2
 -- Revision  1 - File Created
--- 
+--           2 - Simplified using unconstrained ports and Vivado built-in functions
+--               Based heavily on code written by user "asp_digital" on Reddit
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity par2ser is
-	generic (
-		DATA_W		: natural := 8
-	);
-	port (
-		clk       :  in std_logic;
-		
-		-- inputs
-		data_in   :	 in std_logic_vector (DATA_W-1 downto 0);
-		load      :  in std_logic;
-		
-		-- outputs
-		data_out  : out std_logic;
-		busy      :	out std_logic := '0';
-		valid     :	out std_logic := '0';
-		frame     :	out std_logic := '0'
-	);
+  port (
+    clk      : in  std_logic;
+
+    -- inputs
+    data_in  : in  std_logic_vector;
+    load     : in  std_logic;
+
+    -- outputs
+    data_out : out std_logic;
+    busy     : out std_logic := '0';
+    frame    : out std_logic := '0'
+  );
 end par2ser;
-
-
 architecture rtl of par2ser is
-  function log2ceil (a : natural) return integer is
-    variable calc : integer := 0;
-    variable inp  : integer := a;
+  signal cnt : natural range 0 to data_in'left;
+  signal reg : std_logic_vector (data_in'left downto 0) := (others => '0');
+
+begin
+
+  par2ser_pr : process (clk)
   begin
-    while (inp > 0) loop
-	    inp := inp/2;
-	    if (calc < 31) then
-        calc   := calc + 1;
-      end if;
-    end loop;
-    return calc;
-  end function log2ceil;
-
-	constant CNT_W  : natural := log2ceil(DATA_W);	-- calculate needed width of internal counter
-	signal cnt 		  : unsigned (CNT_W-1 downto 0) := (others => '0');
-	signal reg 		  : std_logic_vector (DATA_W-1 downto 0) := (others => '0');
-	signal frame_s 	: std_logic_vector (1 downto 0);
-
-begin 
-
-  par2ser_pr: process (clk) 
-  begin 
     if (rising_edge(clk)) then
-      if (load = '1') then				-- load parallel register
-        reg		<= data_in;
-        cnt 	<= to_unsigned(DATA_W-1, cnt'length);
+      -- the bit counter, for framing, always counts down to zero and then stops.
+      -- The counter is preset when the parallel register is loaded.
+      BitCounter : if cnt > 0 then
+        cnt <= cnt - 1;
+      end if BitCounter;
+
+      -- busy flag should be cleared when not shifting.
+      -- It gets set immediately on load, overriding this.
+      EndBusy : if cnt = 0 then
+        busy <= '0';
+      end if EndBusy;
+
+      ParallelReg : if (load = '1') then
+        reg <= data_in; -- load parallel register
+        cnt <= data_in'left; -- size of the parallel register
+        busy <= '1'; -- ensure immediate busy flag
+      end if ParallelReg;
+
+      -- frame strobe goes true when the count reaches zero, and is cleared right away.
+      -- Let's take advantage of pipelining.
+      AssertFrameStrobe : if cnt = 1 then
+        frame <= '1';
       else
-        if (cnt > 0) then				-- all data bits transferred?
-          cnt		<= cnt - 1;			
-        end if;	
-      end if;	
+        frame <= '0';
+      end if AssertFrameStrobe;
+
     end if;
   end process par2ser_pr;
 
-  data_out <= reg(to_integer(cnt));
-
-  -- generate single clock pulse for frame
-  frame_pr: process (clk) 
-	begin 
-    if (rising_edge(clk)) then
-      frame_s(1) <= frame_s(0);		
-    end if;
-  end process;
-
-  frame_s(0) <= '1' when (cnt = 0) else '0';
-  frame <= '1' when frame_s = "01" else '0';
-  busy  <= '1' when (cnt > 0) or frame='1' else '0';
-  valid <= busy;
+  data_out <= reg(cnt);
 
 end rtl;
 
